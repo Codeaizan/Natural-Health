bfa6ff86acadad0b12c9c72b3aad6f884cea87ec
Faizanur Rahman <faizanrahman51@gmail.com>
2026-09-23 22:36:13 +0530

    fix: resolve missing image import in Products.jsx

--- src/pages/Products.jsx
+++ src/pages/Products.jsx
@@ -2,7 +2,7 @@ import { useState } from 'react';
 import HeroBanner from '../components/HeroBanner';
 import AnimatedSection from '../components/AnimatedSection';
 import { allProducts as products, categories } from '../data/products';
-import productsCollectionImg from '../assets/images/products-collection.jpg';
+import productsCollectionImg from '../assets/images/home-hero.jpg';
 import { FaLeaf } from 'react-icons/fa';
 import './Products.css';
 
